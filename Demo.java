a55fca85a68bedd59978e2d137819ece64a520d8
meghana123-k <kothakondameghana3@gmail.com>
2025-07-21 16:44:29 +0530

    Worked on Basics of Java

--- Demo.java
+++ Demo.java
@@ -1,15 +1,23 @@
 class Demo {
 	int i = 10;
-	//public Demo() {
-		//System.out.println("this.i = "+this.i);
-	//}
+	public Demo() {
+		System.out.println("this.i = "+this.i);
+	}
 	public Demo(int i) {
 		this.i = i;
 		System.out.println("this.i = "+this.i);
 	}
 	
 	public static void main(String... args) {
-		Demo d1 = new Demo();
-		//Demo d2 = new Demo(100);
+		Demo d1 = new Demo(100);
+		byte x = 127;
+		boolean b1= true;
+		double d = 1.2D;
+		System.out.println(x);
+		System.out.println(b1);
+		System.out.println(d);
+		int a = 10, b = 017, c = 0X1A;
+		System.out.println(a + "," + b + "," + c);
+		System.out.println("Hello World");
 	}
 }
