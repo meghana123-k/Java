60d0a8fe3496c6938bf2d71effd9c6eab030ae5b
meghana123-k <kothakondameghana3@gmail.com>
2025-07-29 11:12:41 +0530

    written cla3 program

--- Assignments/Java Fundamentals/CLA3.java
+++ Assignments/Java Fundamentals/CLA3.java
@@ -1,4 +1,12 @@
 
 class CLA3 {
-    
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+        } else {
+            System.out.println("You did not entered company and location inputs");
+        }
+    }
 }
