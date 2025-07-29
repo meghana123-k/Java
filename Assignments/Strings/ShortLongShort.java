e9bdf37f9155f0146fb691d7f3ba74b3ab457c4f
meghana123-k <kothakondameghana3@gmail.com>
2025-07-29 17:33:58 +0530

    short+Long+short of 2 strings

--- Assignments/Strings/ShortLongShort.java
+++ Assignments/Strings/ShortLongShort.java
@@ -1,3 +1,9 @@
 class ShortLongShort {
-    
+    public static void main(String[] args) {
+        String s1 = "hi";
+        String s2 = "hello";
+        int n = s1.length();
+        int m = s2.length();
+        System.out.println(n > m ? s2+s1+s2 : s1+s2+s1);
+    }
 }
