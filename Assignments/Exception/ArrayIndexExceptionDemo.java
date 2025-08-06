53b505d05d956539e370542429299ddb17f27853
meghana123-k <kothakondameghana3@gmail.com>
2025-08-06 22:53:53 +0530

    practised array index out of exception program using try catch

---
+++ Assignments/Exception/ArrayIndexExceptionDemo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class ArrayIndexExceptionDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter n: ");
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            System.out.println("Enter elements ");
+            for (int i = 0; i < a.length; i++) {
+                a[i] = sc.nextInt();
+            }
+            System.out.println("Enter index you want to access ");
+            int idx = sc.nextInt();
+            System.out.println("At " + idx + "th index, the element is " + a[idx]);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
